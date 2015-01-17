7e23a17614a6b479e5bba2e34325e5879cf57624
dsernst <github@dsernst.com>
2015-01-17 11:33:07 -0800

    implements RegExp username & password validator

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -5,26 +5,43 @@ angular.module('shortly.auth', [])
 
 .controller('AuthController', function ($scope, $window, $location, Auth) {
   $scope.user = {};
-
+  var usernameAllowed = /^(\w|\d){1,30}$/i;
+  var passwordAllowed = /^.{1,50}$/i;
   $scope.signin = function () {
-    Auth.signin($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    if($scope.user.username.match(usernameAllowed)) {
+      if ($scope.user.password.match(passwordAllowed)) {
+        Auth.signin($scope.user)
+          .then(function (token) {
+            $window.localStorage.setItem('com.shortly', token);
+            $location.path('/links');
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      } else {//password is invalid
+        console.error('your password is too long!');
+      }
+    } else {//username is invalid
+      console.error('username has invalid characters');
+    }
   };
 
   $scope.signup = function () {
-    Auth.signup($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    if($scope.user.username.match(usernameAllowed)) {
+      if ($scope.user.password.match(passwordAllowed)) {
+        Auth.signup($scope.user)
+          .then(function (token) {
+            $window.localStorage.setItem('com.shortly', token);
+            $location.path('/links');
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      } else {
+        console.error('your password is too long!');
+      }
+    } else {
+      console.error('username has invalid characters');
+    }
   };
 });
